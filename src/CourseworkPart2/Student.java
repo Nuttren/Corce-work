6927016484c57260e53780e92212b90547691db0
nuttren <roman.karpencko@gmail.com>
2023-03-28 18:36:38 +0400

    Course work 2

---
+++ src/CourseworkPart2/Student.java
@@ -0,0 +1,21 @@
+package CourseworkPart2;
+
+import java.util.Objects;
+
+public class Student {
+    private String name;
+    private int age;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Student)) return false;
+        Student student = (Student) o;
+        return age == student.age && Objects.equals(name, student.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, age);
+    }
+}
